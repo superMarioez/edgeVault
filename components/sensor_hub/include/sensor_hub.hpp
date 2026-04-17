a2483d0d4a4be74f42c770a23503eda54b80e550
superMarioez <omarkhalid881@gmail.com>
2026-04-17 17:35:46 +0200

    Refactor sensor hub integration: update CMakeLists, enhance Kconfig for DS3231 address, and improve main.cpp structure with I2C master bus initialization

--- components/sensor_hub/include/sensor_hub.hpp
+++ components/sensor_hub/include/sensor_hub.hpp
@@ -1,9 +1,15 @@
 #pragma once
 
 #include "ev_types.hpp"
+#include "driver/i2c_master.h"
 
 namespace sensorhub {
 
     void local_sensor_task(void *pvParameters);
+
+    struct SensorTaskContext {
+        i2c_master_bus_handle_t i2c_handle_;
+        EventGroupHandle_t system_events_handle_;
+    };
 
 }
--- main/main.cpp
+++ main/main.cpp
@@ -5,21 +5,42 @@
 #include "data_pipeline.hpp"
 #include "network_manager.hpp"
 
-static const char* TAG = "edgevault";
+namespace {
+const char* TAG = "edgevault";
+sensorhub::SensorTaskContext sensor_hub_data = {};
+}
 
 extern "C" void app_main(void)
 {
 
     ESP_LOGI(TAG, "EdgeVault v0.1.0");
 
+    // create the system event group
     EventGroupHandle_t system_events_handle = xEventGroupCreate();
     ESP_ERROR_CHECK(system_events_handle ? ESP_OK : ESP_ERR_NO_MEM);
+
+    // initializing the i2c master bus
+    i2c_master_bus_config_t i2c_bus_cfg = {};
+    i2c_bus_cfg.i2c_port = I2C_NUM_0;
+    i2c_bus_cfg.sda_io_num = static_cast<gpio_num_t>(CONFIG_EV_I2C_SDA_GPIO);
+    i2c_bus_cfg.scl_io_num = static_cast<gpio_num_t>(CONFIG_EV_I2C_SCL_GPIO);
+    i2c_bus_cfg.flags.enable_internal_pullup = true;
+    i2c_bus_cfg.flags.allow_pd = false;
+    i2c_bus_cfg.glitch_ignore_cnt = 7;
+    i2c_bus_cfg.clk_source = I2C_CLK_SRC_DEFAULT;
+
+    sensor_hub_data.system_events_handle_ = system_events_handle;
+
+    // create the i2c master bus
+    ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &sensor_hub_data.i2c_handle_));
+
+
 
     configASSERT(xTaskCreatePinnedToCore(
         sensorhub::local_sensor_task,
         "sensor_hub",
         ev::config::TASK_STACK_SIZE_DEFAULT,
-        (void*)system_events_handle,
+        static_cast<void*>(&sensor_hub_data),
         5,
         nullptr,
         1
@@ -29,7 +50,7 @@ extern "C" void app_main(void)
         datapipeline::data_pipeline_task,
         "data_pipe",
         ev::config::TASK_STACK_SIZE_DEFAULT,
-        (void*)system_events_handle,
+        static_cast<void*>(system_events_handle),
         4,
         nullptr,
         1
@@ -39,7 +60,7 @@ extern "C" void app_main(void)
         networkmanager::network_manager_task,
         "net_mgr",
         ev::config::TASK_STACK_SIZE_DEFAULT,
-        (void*)system_events_handle,
+        static_cast<void*>(system_events_handle),
         6,
         nullptr,
         0
