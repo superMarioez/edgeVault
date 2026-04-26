ec7c3434b7346028af4f6257fbf350ca9a991e55
superMarioez <omarkhalid881@gmail.com>
2026-04-26 19:03:05 +0300

    Refactor data pipeline logging: introduce file_log_row function and update log file handling

--- components/data_pipeline/src/data_pipeline.cpp
+++ components/data_pipeline/src/data_pipeline.cpp
@@ -10,6 +10,7 @@ namespace datapipeline {
 
     static const char *TAG = "data pipeline";
 
+    constexpr const char* MOUNT_PATH = "/sdcard";
 
     static constexpr std::array<const char*, static_cast<size_t>(ev::SensorSource::Count)> nvs_keys = 
     {
@@ -21,6 +22,27 @@ namespace datapipeline {
     };
     static_assert(nvs_keys.size() == static_cast<size_t>(ev::SensorSource::Count),
     "nvs_keys must match SensorSource count");
+
+    static void file_log_row(FILE* file_h, DataPipelineContext* dp_context, ev::SensorReading ssr_rdgs) {
+        int fret = fprintf(
+            file_h,
+            "%" PRId64 ",%s,%0.2f,%s,%s,%d\n",
+            ssr_rdgs.timestamp_ms_,
+            ev::ssr_id_to_str(ssr_rdgs.sensor_).sensor,
+            ssr_rdgs.value_,
+            ev::ssr_id_to_str(ssr_rdgs.sensor_).unit,
+            ev::quality_to_str(ssr_rdgs.quality_),
+            ssr_rdgs.register_addr_);
+
+        if (fret < 0) {
+            ESP_LOGE(TAG, "data row logging failed");
+            xEventGroupSetBits(
+                dp_context->system_events_h_,
+                ev::to_bits(ev::SystemEvent::StorageError)
+            );
+        }
+        
+    }
 
     void data_pipeline_task(void *pvParameters) {
 
@@ -40,15 +62,39 @@ namespace datapipeline {
             }
         }
 
+    {
+
         // create an sd logger
-        sdlogger::SDLogger logger(dp_ctx->spi_host_, "/sdcard");
+        sdlogger::SDLogger logger(dp_ctx->spi_host_, MOUNT_PATH);
+
+        char file_path[64];
+        snprintf(file_path, sizeof(file_path), "%s/log.csv", MOUNT_PATH);
 
         // open a csv file
-        FILE* log_file = fopen("/sdcard/log.csv", "a+");
+        FILE* log_file = fopen(file_path, "a+");
 
-        if (log_file != nullptr) {
-            if (!ftell(log_file)) {
-                fprintf(log_file, "timestamp_ms,sensor,value,quality,register_addr\n");
+        int fret = -1;
+
+        if (log_file == nullptr) ESP_LOGE(TAG, "couldn't open log file");
+        else {
+            // placing the csv header
+            fret = fseek(log_file, 0, SEEK_END);
+            if (fret < 0) ESP_LOGE(TAG, "file cursor cannot be moved to the EOF");
+
+            long size = ftell(log_file);
+            if (size == 0) {
+                ESP_LOGI(TAG, "new CSV file created, printing header");
+
+                fret = fprintf(log_file, "timestamp_ms,sensor,value,unit,quality,register_addr\n");
+                if (fret < 0) ESP_LOGE(TAG, "file header cannot be printed");
+                
+                else {
+                    fret = fflush(log_file);
+                    if (fret < 0) {
+                        ESP_LOGE(TAG, "header logging failed");
+                        xEventGroupSetBits(dp_ctx->system_events_h_, ev::to_bits(ev::SystemEvent::StorageError));
+                    }
+                }
             }
         }
 
@@ -77,23 +123,37 @@ namespace datapipeline {
                     ssr_buff.timestamp_ms_
                     ,ev::ssr_id_to_str(ssr_buff.sensor_).sensor
                 );
+
+                if (log_file != nullptr) {
+                    file_log_row(log_file, dp_ctx, ssr_buff);
+                }
             }
 
             else {
 
-                if (std::isnan(ssr_buff.value_))
+                if (std::isnan(ssr_buff.value_)) {
                     ESP_LOGE(
                         TAG,
                         "[%" PRId64 "] %s | NAN",
                         ssr_buff.timestamp_ms_,
                         ev::ssr_id_to_str(ssr_buff.sensor_).sensor
                     );
-                    
+
+                if (log_file != nullptr) {
+                    file_log_row(log_file, dp_ctx, ssr_buff);
+                    }
+                }
+            
                 else {
                     ssr_buff.value_ += offsets[static_cast<uint8_t>(ssr_buff.sensor_)];
+                    
+                    if (log_file != nullptr) {
+                        file_log_row(log_file, dp_ctx, ssr_buff);
+                    }
+
                     ESP_LOGI(
                         TAG,
-                        "[%" PRId64 "] %s | value = %0.2f %s | quality= %s ",
+                        "%" PRId64 " %s | value = %0.2f %s | quality= %s ",
                         ssr_buff.timestamp_ms_,
                         ev::ssr_id_to_str(ssr_buff.sensor_).sensor,
                         ssr_buff.value_,
@@ -103,6 +163,14 @@ namespace datapipeline {
                 }
             }
         }
+
+        fret = fclose(log_file);
+        if (fret < 0) ESP_LOGE(TAG, "log file couldn't be closed");
     }
 
+    vTaskDelete(nullptr);
+}
+
+
+    
 }
